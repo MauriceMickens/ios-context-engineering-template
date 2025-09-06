6e16288b5c9c3b3b373138ba8eea649a5a5e4c4c
mickens <mauricetmickens@gmail.com>
2025-09-06 11:53:52 -0400

    Initial Commit

---
+++ context-eng-template/context_eng_templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  context_eng_templateApp.swift
+//  context-eng-template
+//
+//  Created by Mickens on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct context_eng_templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ context-eng-templateTests/context_eng_templateTests.swift
@@ -0,0 +1,17 @@
+//
+//  context_eng_templateTests.swift
+//  context-eng-templateTests
+//
+//  Created by Mickens on 9/6/25.
+//
+
+import Testing
+@testable import context_eng_template
+
+struct context_eng_templateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
